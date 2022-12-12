7cc8498ebfbbd1aec503915770d227e2d5080761
Wadjid <wadjidassani@gmail.com>
2022-12-12 11:54:48 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
- * main print a given phrase 
- * Return: 0 if exited properly
+ * main Entry
+ * Return: Always 0
  */
-
 int main(void)
 {
-	puts("\"Programming is like a building a multilingual puzzle"); 
+	puts ("\"Programming is like a building a multilingual puzzle"); 
 	return(0);
 }
 
