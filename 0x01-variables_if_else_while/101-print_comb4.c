ef01fcb46b4c7ef89785acf56ea65fc2d8781645
Wadjid <wadjidassani@gmail.com>
2022-12-12 18:19:01 -0800

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -23,8 +23,12 @@ int main(void)
 						putchar(j);
 						putchar(k);
 
-						if (i == 55 && j == 56 && k == 57)
+						if (i == 55)
 						{
+							if (j == 56)
+							{
+								if (k == 57)
+							}
 							break;
 						}
 						putchar(',');
