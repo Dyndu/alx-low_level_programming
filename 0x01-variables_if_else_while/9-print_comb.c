46395a388c10933434a340caeeaacc11f27cdb6c
Wadjid <wadjidassani@gmail.com>
2022-12-12 17:15:27 -0800

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main -prints all combnations of single digit 
+ * main -prints all combnations of single digit
  * Return:Always 0
  */
 int main(void)
 {
-	int n; 
+	int n;
 
 	for (n = 48; n < 58; n++)
 	{
@@ -17,6 +17,6 @@ int main(void)
 		}
 	}
 
-	putchar('\n')
+	putchar('\n');
 	return (0);
 }
