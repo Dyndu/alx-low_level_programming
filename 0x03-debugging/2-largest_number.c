034a324be76fd9479ba564b57f6fda4dfdf45126
Wadjid <wadjidassani@gmail.com>
2022-12-15 08:25:36 -0800

    Check the largest between 3 numbers

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * main - largest_number - return the largest between 3 numbers
+ * @a: first number
+ * @b: second number
+ * @c: third number
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+int largest;
+
+if (a > b)
+{
+if (a > c)
+{
+largest = a;
+}
+else
+{
+largest = c;
+}
+}
+else if (b > c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
+
+return (largest);
+}
